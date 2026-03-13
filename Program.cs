6e9f2a3b672cecbb9d90ccbb36876bf480838ca2
Suraj <surajyadu.9@gmail.com>
2026-03-14 01:04:23 +0530

    connected postgres database

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 
 // Database connection
 builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // CORS
 builder.Services.AddCors(options =>
